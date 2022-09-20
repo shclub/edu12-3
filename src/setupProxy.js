54be42abb4427cd642a03a5bce36679eca4a01db
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-09-20 17:18:43 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
     createProxyMiddleware('/', {
-      target: 'http://backend:8080',
+      target: 'http://backend',
       #target: process.env.REACT_APP_API_URL,
       changeOrigin: true
     })
