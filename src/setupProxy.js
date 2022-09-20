c056bb429d766401ac6c5b4ada92f001db00059c
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-09-20 18:44:34 +0900

    Create setupProxy.js

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+ module.exports = function(app){
+   app.use(
+     createProxyMiddleware('/', {
+       target: 'http://backend',
+       #target: process.env.REACT_APP_API_URL,
+       changeOrigin: true
+     })
+   )
+
+ };
+ #출처: https://woochan-dev.tistory.com/94 [우찬쓰 개발블로그:티스토리]
